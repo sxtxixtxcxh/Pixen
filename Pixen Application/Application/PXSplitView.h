e9436bf9ee01daad8fb51aead37ef8d860cafba9
Matt Rajca <matt.rajca@me.com>
2011-06-21 13:44:43 -0500

    Use a thin divider style between the sidebar and canvas panes

---
+++ Pixen Application/Application/PXSplitView.h
@@ -0,0 +1,15 @@
+//
+//  PXSplitView.h
+//  Pixen
+//
+//  Created by Matt Rajca on 6/21/11.
+//  Copyright 2011 Matt Rajca. All rights reserved.
+//
+
+// Draws a darker divider
+
+#import <AppKit/NSSplitView.h>
+
+@interface PXSplitView : NSSplitView
+
+@end
---
+++ Pixen Application/Application/PXSplitView.m
@@ -0,0 +1,18 @@
+//
+//  PXSplitView.m
+//  Pixen
+//
+//  Created by Matt Rajca on 6/21/11.
+//  Copyright 2011 Matt Rajca. All rights reserved.
+//
+
+#import "PXSplitView.h"
+
+@implementation PXSplitView
+
+- (NSColor *)dividerColor
+{
+	return [NSColor colorWithDeviceWhite:0.4f alpha:1.0f];
+}
+
+@end
