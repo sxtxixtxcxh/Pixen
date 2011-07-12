a433288e23267a01ac0ee17ae374a86fe0636c1c
Matt Rajca <matt.rajca@me.com>
2011-07-12 12:47:38 -0500

    Added the initial implementation of a WebKit-based Welcome screen

--- Pixen Application/Welcome/PXWelcomeController.h
+++ Pixen Application/Welcome/PXWelcomeController.h
@@ -1,61 +1,20 @@
 //
 //  PXWelcomeController.h
-//  Pixen-XCode
-
-// Copyright (c) 2003,2004,2005 Open Sword Group
-
-// Permission is hereby granted, free of charge, to any person obtaining a copy
- 
-// of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation 
-// the rights  to use,copy, modify, merge, publish, distribute, sublicense, 
-// and/or sell copies of the Software, and to permit persons to whom
-//  the Software is  furnished to do so, subject to the following conditions:
-
-// The above copyright notice and this permission notice shall be included in
-//  all copies or substantial portions of the Software.
-
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
-// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
-// BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
-// CONTRACT, TORT OR OTHERWISE, ARISING FROM,  OUT OF OR IN CONNECTION WITH
-// THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-//  Created by Andy Matuschak on Sat Jun 12 2004.
-//  Copyright (c) 2004 Open Sword Group. All rights reserved.
+//  Pixen
+//
+//  Copyright Matt Rajca 2011. All rights reserved.
 //
 
-// Based upon UKPrefsPanel by M. Uli Kusterer
-
-#import <AppKit/AppKit.h>
-
+#import <WebKit/WebKit.h>
 
 @interface PXWelcomeController : NSWindowController
 {
   @private
-	IBOutlet NSTabView*		tabView;			// The tabless tab-view that we're a switcher for.
-	IBOutlet id image;
-	IBOutlet id next, prev, close;
-	
-	// Auto-generated from tab view's items.
-	NSMutableDictionary*	itemsList;		
-    // Auto-fetched at awakeFromNib time. We append a colon and the name 
-	// of the current page to the actual window title.	
-	NSString*				baseWindowName;
+	WebView *webView;
 }
 
-+(id) sharedWelcomeController;
+@property (assign) IBOutlet WebView *webView;
 
-// Accessors for specifying the tab view: (you should just hook these up in IB)
--(void)			setTabView: (NSTabView*)tv;
--(NSTabView*)   tabView;
-
--(IBAction)	changePanes: (id)sender;
-
--(IBAction)next:(id) sender;
--(IBAction)prev:(id) sender;
-
++ (id)sharedWelcomeController;
 
 @end
