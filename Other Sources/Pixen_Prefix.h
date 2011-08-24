e5a31b5be15b7a1310c01d80c819bbc5792bc9ea
Matt Rajca <matt.rajca@me.com>
2011-08-24 08:05:14 -0500

    Revised ARC-related preprocessor statements

--- Other Sources/Pixen_Prefix.h
+++ Other Sources/Pixen_Prefix.h
@@ -2,18 +2,18 @@
 // Prefix header for all source files of the 'Pixen' target in the 'Pixen' project
 //
 
-#ifndef __bridge
-#define __bridge
-#endif
-
-#ifndef __unsafe_unretained
-#define __unsafe_unretained
-#endif
-
 #ifdef __OBJC__
     #import <Cocoa/Cocoa.h>
     #import "NSArray_DeepMutableCopy.h"
     #import "PXNotifications.h"
     #import "PXDefaults.h"
     #import "Constants.h"
+#endif
+
+#if !(__has_feature(objc_arc))
+#define __bridge
+#endif
+
+#if !(__has_feature(objc_arc))
+#define __unsafe_unretained
 #endif
