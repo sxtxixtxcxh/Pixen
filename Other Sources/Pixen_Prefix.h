d24f4449065c42411a52fb335cc81a7c48da7784
Matt Rajca <matt.rajca@me.com>
2011-08-25 10:06:33 -0500

    Use __has_feature to detect if ARC is enabled

--- Other Sources/Pixen_Prefix.h
+++ Other Sources/Pixen_Prefix.h
@@ -2,18 +2,20 @@
 // Prefix header for all source files of the 'Pixen' target in the 'Pixen' project
 //
 
+#ifndef __has_feature
+#define __has_feature(x) 0
+#endif
+
+#if !__has_feature(objc_arc)
+#define __unsafe_unretained
+#define __bridge
+#endif
+
+
 #ifdef __OBJC__
     #import <Cocoa/Cocoa.h>
     #import "NSArray_DeepMutableCopy.h"
     #import "PXNotifications.h"
     #import "PXDefaults.h"
     #import "Constants.h"
-#endif
-
-#if !(__has_feature(objc_arc))
-#define __bridge
-#endif
-
-#if !(__has_feature(objc_arc))
-#define __unsafe_unretained
 #endif
