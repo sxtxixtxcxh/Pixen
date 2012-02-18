5e4aba1afca96b6acc40a5555b216cd880b140e2
Matt Rajca <matt.rajca@me.com>
2012-02-18 12:25:02 -0600

    Added a 'Tests' target

---
+++ Tests/PaletteTests.h
@@ -0,0 +1,12 @@
+//
+//  PaletteTests.h
+//  Pixen
+//
+//  Copyright 2012 Pixen Project. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface PaletteTests : SenTestCase
+
+@end
---
+++ Tests/PaletteTests.m
@@ -0,0 +1,31 @@
+//
+//  PaletteTests.m
+//  Pixen
+//
+//  Copyright 2012 Pixen Project. All rights reserved.
+//
+
+#import "PaletteTests.h"
+
+@implementation PaletteTests
+
+- (void)setUp
+{
+	[super setUp];
+	
+	// Set-up code here.
+}
+
+- (void)tearDown
+{
+	// Tear-down code here.
+	
+	[super tearDown];
+}
+
+- (void)testExample
+{
+	// STFail(@"Unit tests are not implemented yet in Tests");
+}
+
+@end
