740ede507bdb57770762ece8481d151098c7ec92
Matt Rajca <matt.rajca@me.com>
2012-06-08 12:48:41 -0500

    More formal comparison of UTIs

--- Other Sources/OSG/Utility/PathUtilities.m
+++ Other Sources/OSG/Utility/PathUtilities.m
@@ -58,32 +58,36 @@ NSString *GetPixenPatternFile()
 
 NSString *GetDescriptionForDocumentType(NSString *uti)
 {
-	if ([uti isEqualToString:@"com.Pixen.pxim"]) {
+	CFStringRef cfUTI = (CFStringRef) uti;
+	
+	if (UTTypeEqual(cfUTI, CFSTR("com.Pixen.pxim"))) {
 		return @"Pixen image";
 	}
-	else if ([uti isEqualToString:@"com.Pixen.pxan"]) {
+	else if (UTTypeEqual(cfUTI, CFSTR("com.Pixen.pxan"))) {
 		return @"Pixen animation";
 	}
-	else if ([uti isEqualToString:@"com.compuserve.gif"]) {
+	else if (UTTypeEqual(cfUTI, CFSTR("com.compuserve.gif"))) {
 		return @"GIF image";
 	}
-	else if ([uti isEqualToString:@"public.jpeg"]) {
+	else if (UTTypeEqual(cfUTI, CFSTR("public.jpeg"))) {
 		return @"JPEG image";
 	}
-	else if ([uti isEqualToString:@"public.png"]) {
+	else if (UTTypeEqual(cfUTI, CFSTR("public.png"))) {
 		return @"PNG image";
 	}
-	else if ([uti isEqualToString:@"public.tiff"]) {
+	else if (UTTypeEqual(cfUTI, CFSTR("public.tiff"))) {
 		return @"TIFF image";
 	}
-	else if ([uti isEqualToString:@"com.microsoft.bmp"]) {
+	else if (UTTypeEqual(cfUTI, CFSTR("com.microsoft.bmp"))) {
 		return @"BMP image";
 	}
-	else if ([uti isEqualToString:@"com.microsoft.ico"]) {
+	else if (UTTypeEqual(cfUTI, CFSTR("com.microsoft.ico"))) {
 		return @"ICO image";
 	}
 	
-	@throw [NSException exceptionWithName:NSGenericException reason:@"Invalid type" userInfo:nil];
+	@throw [NSException exceptionWithName:NSGenericException
+								   reason:[NSString stringWithFormat:@"Invalid type (%@)", uti]
+								 userInfo:nil];
 	
 	return nil;
 }
