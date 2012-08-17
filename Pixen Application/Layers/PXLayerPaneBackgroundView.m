b3f3944109b5c2822751825da7b6d3819c7cc77f
Matt Rajca <matt.rajca@me.com>
2012-08-16 19:02:44 -0500

    color tweak

--- Pixen Application/Layers/PXLayerPaneBackgroundView.m
+++ Pixen Application/Layers/PXLayerPaneBackgroundView.m
@@ -16,7 +16,7 @@
 	CGFloat positions[4] = { 0.0f, 11.5f / 23, 11.5f / 23, 1.0f };
 	
 	NSColor *color1 = [NSColor colorWithCalibratedRed:0.90f green:0.90f blue:0.90f alpha:1.0f];
-	NSColor *color2 = [NSColor colorWithCalibratedRed:0.92f green:0.92f blue:0.92f alpha:1.0f];
+	NSColor *color2 = [NSColor colorWithCalibratedRed:0.93f green:0.93f blue:0.93f alpha:1.0f];
 	NSColor *color3 = [NSColor colorWithCalibratedRed:0.84f green:0.84f blue:0.84f alpha:1.0f];
 	NSColor *color4 = [NSColor colorWithCalibratedRed:0.95f green:0.95f blue:0.95f alpha:1.0f];
 	
